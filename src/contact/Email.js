7e5bf8f08247799459b670ec2cb53e12c3a7aed4
Chase Miller <46936509+ChaseMillers@users.noreply.github.com>
2021-02-05 13:44:19 -0800

    Add files via upload

---
+++ src/contact/Email.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Contact.css"
+
+const Email = () => {
+
+    return (
+      
+                <div className="comp-info">
+                    <a href="tel:+4252138204" rel="noopener noreferrer">
+                        (425) 213-8204</a>
+                    <br />
+                    <a href="mailto:eastsideweddingvideo@gmail.com"
+                        rel="noopener noreferrer"
+                        target="_blank">eastsideweddingvideo@gmail.com</a>
+                </div>
+    );
+}
+export default Email;
