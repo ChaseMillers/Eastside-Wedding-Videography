6bb1abb30274fff92b17ec63649de947d39837ee
Chase Miller <46936509+ChaseMillers@users.noreply.github.com>
2021-02-07 09:22:46 -0800

    Update Testimonials.js

--- src/testimonials/Testimonials.js
+++ src/testimonials/Testimonials.js
@@ -68,9 +68,7 @@ const Testimonials = () =>{
                     He is so friendly and an excellent videographer. He captured 
                     all of our moments from the vows to the Newlywed Game, 
                     providing us with a shorter summary video as well as the long 
-                    form of all the different events. He has a lot of video editing 
-                    experience that made the video look just like how we pictured our 
-                    wedding day. We are so thankful to have the memories forever to look back on!"
+                    form of all the different events."
                 </p>
                 <p className="quoter">- Nathan & Acacia </p>
             </div>
@@ -82,10 +80,7 @@ const Testimonials = () =>{
             <h1 className="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</h1>
             <div className="quote">
                 <p>
-                    "Thanks to Greg, we can always pull out 
-                    our wedding video to watch the pure magic of sharing vows, 
-                    the tear jerking toasts, and the amazing people that came to 
-                    support us. Greg not only caught the details and wedding 
+                    "Greg not only caught the details and wedding 
                     ceremony but also the “behind the scenes” parts that make our 
                     relationship special, he also worked alongside our photographers 
                     with no issues. We love Greg’s work and are thankful to have 
