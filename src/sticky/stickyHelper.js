05c95764827ed3ea63321cdcfddb7cf7d0e6f8a9
Chase Miller <chasemillers@outlook.com>
2021-04-25 21:35:50 -0700

    simplified sticky

--- src/sticky/stickyHelper.js
+++ src/sticky/stickyHelper.js
@@ -3,24 +3,9 @@ import './stickyHelper.css'
 import Menu from '../menu/Menu'
 
 const StickyHeader = () => {
-  const [isSticky, setSticky] = useState(false);
-  const ref = useRef(null);
-  const handleScroll = () => {
-    if (ref.current){
-    setSticky(ref.current.getBoundingClientRect().top <= 0);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', () => handleScroll);
-    };
-  }, []);
 
   return (
-    <div className={`sticky-wrapper${isSticky ? ' sticky' : ''}`} ref={ref}>
+    <div className="sticky">
     <Menu />
     </div>
   );
