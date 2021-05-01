be48bd00cb89dc573cbfce4df9a329c5e70dd1c3
Chase Miller <chasemillers@outlook.com>
2021-04-30 17:44:12 -0700

    holding menu with flex

--- src/logo/Logo.js
+++ src/logo/Logo.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
     return(
+        <div className="logo-holder">
             <Link className="logo" to="/">
                 <h1>Eastside Wedding Videography</h1>
             </Link>
+        </div>
     )
 }
 
